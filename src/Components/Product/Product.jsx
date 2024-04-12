a708f0ac917f2dcb161ba2df033f448e84a46d5a
Abhishek2310k <abhishekku@iitbhilai.ac.in>
2024-04-12 13:42:59 +0530

    added images

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -7,6 +7,20 @@ const Product = ({ messages }) => {
     const tags = ["Clutch", "Fabric Lining", "Baggit", "Multipurpose", "Krunk"];
     const filters = ["strap-long", "Colour", "Size", "Brand", "Material"];
 
+
+    const getCurrentTime = () => {
+        const date = new Date();
+        let hours = date.getHours();
+        const minutes = date.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; 
+        const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
+        return formattedTime;
+      };
+    
+      const currentTime = getCurrentTime();
+
     return (
         <div className={`product ${messages.length > 0 ? 'show' : ''}`}>
             <div className='r1'>
@@ -42,6 +56,9 @@ const Product = ({ messages }) => {
                     <div className='desc'>
                         Or set Filters and help us choose the best bags for you
                     </div>
+                </div>
+                <div className='time' style={{paddingLeft:"10px",fontSize:"10px"}}>
+                    {currentTime}
                 </div>
             </div>
             <div className='r3'>
